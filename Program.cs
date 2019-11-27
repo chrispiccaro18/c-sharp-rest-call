211bf8b5325458ae1b08dad0c8b2fe6a8e7b894a
chrispiccaro <chrispiccaro18@gmail.com>
2019-11-27 10:16:53 -0800

    this works!

--- Program.cs
+++ Program.cs
@@ -1,28 +1,59 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace rest_call
 {
     class Program
     {
-        // private int maxNumber = 0;
-        // private int currentNumber = 0;
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            ApiHelper.InitializeClient();
-            Console.WriteLine("Hello World!");
+        static HttpClient client = new HttpClient();
 
+        static void ShowStarship(StarshipModel starship)
+        {
+            Console.WriteLine($"Name: {starship.Name}\tClass: " +
+                $"{starship.Starship_Class}");
         }
 
-        // private async Task LoadImage(int imageNumber = 0)
-        // {
-        //     var starship = await StarshipProcessor.LoadStarship(imageNumber);
+        static async Task<StarshipModel> GetStarshipAsync(string path = "")
+        {
+            StarshipModel starship = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                starship = await response.Content.ReadAsAsync<StarshipModel>();
+            }
+            return starship;
+        }
 
-        //     if (imageNumber == 0)
-        //     {
-                
-        //     }
-        // }
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("https://swapi.co/api/starships/15");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Create a new product
+                StarshipModel starship = null;
+
+                starship = await GetStarshipAsync();
+                ShowStarship(starship);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
     }
 }
